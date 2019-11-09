4c3058f0892888fa85f2e0d0d3a0bda0dd3e8d48
landsea <wobushiludahai@163.com>
2019-11-09 11:04:38 +0800

    2019年11月9日11:04:29 timer ok

---
+++ Timer/timer.c
@@ -0,0 +1,10 @@
+/*
+ * @Description: a simple timer frame
+ * @Author: land sea
+ * @Date: 2019-11-08 17:56:13
+ * @LastEditTime: 2019-11-08 18:00:38
+ * @LastEditors: Please set LastEditors
+ */
+
+#include "time.h"
+
---
+++ Timer/timer.h
@@ -0,0 +1,21 @@
+/*
+ * @Description: a simple timer frame
+ * @Author: land sea
+ * @Date: 2019-11-08 17:56:13
+ * @LastEditTime: 2019-11-08 18:03:06
+ * @LastEditors: Please set LastEditors
+ */
+
+#ifndef _FRAME_TIMER_H_
+#define _FRAME_TIMER_H_
+
+#include <stdint.h>
+
+typedef struct timer_t
+{
+    uint32_t timeout;
+    uint32_t 
+}TIMER;
+
+
+#endif
