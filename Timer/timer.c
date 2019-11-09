4a611060c412edac31ac285b2493ab78fa03d359
landsea <wobushiludahai@163.com>
2019-11-09 11:06:07 +0800

    2019年11月9日11:04:29 timer ok

--- Timer/timer.c
+++ Timer/timer.c
@@ -2,9 +2,107 @@
  * @Description: a simple timer frame
  * @Author: land sea
  * @Date: 2019-11-08 17:56:13
- * @LastEditTime: 2019-11-08 18:00:38
+ * @LastEditTime: 2019-11-09 11:05:05
  * @LastEditors: Please set LastEditors
  */
 
-#include "time.h"
+#include "timer.h"
+
+//internal ticks
+static uint64_t _timerticks = 0;
+
+static TIMER *timer_list_head = NULL;
+
+void timer_init(TIMER *timer, void (* timeout_handle)(), uint32_t timeout, uint32_t cycletimes)
+{
+    timer->timeout_handle = timeout_handle;
+    timer->timeout = timeout;
+    timer->cycletimes = cycletimes;
+    timer->next = NULL;
+}
+
+void timer_add(TIMER *timer)
+{
+    TIMER *pttimer;
+
+    pttimer = timer_list_head;
+    
+    while(pttimer)
+    {
+        if(pttimer == timer){
+            timer->timegoal = _timerticks + timer->timeout;
+            return;
+        }
+        pttimer = pttimer->next;
+    }
+    timer->next = timer_list_head;
+    timer_list_head = timer;
+    timer->timegoal = _timerticks + timer->timeout;
+}
+
+int timer_setcycletimes(TIMER *timer, uint32_t cycletimes)
+{
+    TIMER *pttimer;
+
+    pttimer = timer_list_head;
+
+    while(pttimer)
+    {
+        if(pttimer == timer){
+            pttimer->cycletimes = cycletimes;
+            return TIMER_STATECODE_SET_OK;
+        }
+        pttimer = pttimer->next;
+    }
+    return TIMER_STATECODE_NONE_EXIST;
+}
+
+void timer_stop(TIMER *timer)
+{
+    timer->cycletimes = 0;
+}
+
+int timer_remove(TIMER *timer)
+{
+    TIMER *pttimercur, *pttimerpre = NULL;
+
+    pttimercur = timer_list_head;
+    
+    while(pttimercur)
+    {
+        if(pttimercur == timer){
+            if(pttimerpre) pttimerpre->next = pttimercur->next;
+            else timer_list_head = pttimercur->next;    //the timer need to remove is timerlist head
+            return TIMER_STATECODE_DELETE_OK;
+        }
+        pttimerpre = pttimercur;
+        pttimercur = pttimercur->next;
+    }
+    return TIMER_STATECODE_NONE_EXIST;
+}
+
+void timer_loop(void)
+{
+    TIMER *pttimer;
+
+    pttimer = timer_list_head;
+
+    while(pttimer)
+    {
+        if((pttimer->cycletimes > 0)&&(_timerticks >= pttimer->timegoal))
+        {
+            pttimer->timeout_handle();
+            if(--pttimer->cycletimes > 0)
+            {
+                pttimer->timegoal = _timerticks + pttimer->timeout;
+            }
+        }
+        pttimer = pttimer->next;
+    }
+}
+
+void timer_tick(void)
+{
+    _timerticks++;
+}
 
--- Timer/timer.h
+++ Timer/timer.h
@@ -2,7 +2,7 @@
  * @Description: a simple timer frame
  * @Author: land sea
  * @Date: 2019-11-08 17:56:13
- * @LastEditTime: 2019-11-08 18:03:06
+ * @LastEditTime: 2019-11-09 11:05:07
  * @LastEditors: Please set LastEditors
  */
 
@@ -10,12 +10,85 @@
 #define _FRAME_TIMER_H_
 
 #include <stdint.h>
+#include <stddef.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 typedef struct timer_t
 {
     uint32_t timeout;
-    uint32_t 
+    uint64_t timegoal;
+    void     (*timeout_handle)(void);
+    uint32_t cycletimes;
+    TIMER    *next;
 }TIMER;
 
+enum timer_state_code
+{
+    TIMER_STATECODE_ZERO = 0,
+    TIMER_STATECODE_SET_OK,             //set success
+    TIMER_STATECODE_NONE_EXIST,         //timer does not exist
+    TIMER_STATECODE_DELETE_OK,          //delete success
+};
+
+/**
+ * @description: initialize timer param
+ * @param timer: timer struct
+ * @param timeout_handle: timeout handle function
+ * @param timeout: time value
+ * @param cycletimes: time times
+ * @return: None
+ */
+void timer_init(TIMER *timer, void (* timeout_handle)(), uint32_t timeout, uint32_t cycletimes);
+
+/**
+ * @description: add timer struct into the loop list
+ * @param timer:timer struct
+ * @return: None
+ */
+void timer_add(TIMER *timer);
+
+/**
+ * @description: set time times
+ * @param time:timer struct
+ * @param cycletimes:time tines
+ * @return: timer state code
+ */
+int timer_setcycletimes(TIMER *timer, uint32_t cycletimes);
+
+/**
+ * @description: stop one timer in the loop list
+ * @param timer:timer struct
+ * @return: None
+ */
+void timer_stop(TIMER *timer);
+
+/**
+ * @description: remove onr timer fron the loop list
+ * @param timer:timer struct
+ * @return: timer state code
+ */
+int timer_remove(TIMER *timer);
+
+/**
+ * @description: main loop, check the timer list
+ * @param None 
+ * @return: None
+ */
+void timer_loop(void);
+
+/**
+ * @description: use uint64_t, when tick is 1ms, its Inversion stage ≈584942417 years
+ * @param None 
+ * @return: None
+ */
+void timer_tick(void);
+
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
