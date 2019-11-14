9fec80c8e83986254fb514e7238f64bf8c09b517
luhai <2286875743@qq.com>
2019-11-14 23:15:29 +0800

    update button

--- Button/button.c
+++ Button/button.c
@@ -1,7 +1,14 @@
 /*
- * @Description: a simple button frame
+ * @Description: a simple button frame V 1.0
  * @Author: land sea
  * @Date: 2019-11-13 15:46:06
- * @LastEditTime: 2019-11-13 17:38:01
+ * @LastEditTime: 2019-11-14 23:07:40
  * @LastEditors: Please set LastEditors
  */
+
+#include "button.h"
+
+#define 
+
+static BUTTON button_handle = NULL;
+
--- Button/button.h
+++ Button/button.h
@@ -1,8 +1,8 @@
 /*
- * @Description: a simple button frame
+ * @Description: a simple button frame  V 1.0
  * @Author: land sea
  * @Date: 2019-11-13 15:46:15
- * @LastEditTime: 2019-11-13 17:42:21
+ * @LastEditTime: 2019-11-14 23:14:41
  * @LastEditors: Please set LastEditors
  */
 #ifndef _FRAME_BUTTON_H_
@@ -14,8 +14,15 @@
 
 typedef enum button_event_code
 {
-    BUTTON_EVENT_NONE = 0,
-
+    BUTTON_EVENT_PRESS_DOWN = 0,
+    BUTTON_EVENT_PRESS_UP,
+    BUTTON_EVENT_PRESS_DOUBLE,
+    BUTTON_EVENT_SINGLE_CLICK,
+    BUTTON_EVENT_DOUBLE_CLICK,
+    BUTTON_EVENT_LONG_PRESS_START,
+    BUTTON_EVENT_LONG_PRESS_HOLD,
+    number_of_event,
+    BUTTON_EVENT_NONE
 }BUTTON_EVENT;
 
 
