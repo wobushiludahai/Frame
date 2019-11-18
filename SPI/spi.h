02563dfcee71a847cbe4cd602efe9cc7ef8dcb1a
landsea <wobushiludahai@163.com>
2019-11-18 20:59:19 +0800

    spi update

--- SPI/spi.h
+++ SPI/spi.h
@@ -2,7 +2,7 @@
  * @Description: SPI 接口
  * @Author: land sea
  * @Date: 2019-11-18 20:43:34
- * @LastEditTime: 2019-11-18 20:52:44
+ * @LastEditTime: 2019-11-18 20:57:02
  * @LastEditors: Please set LastEditors
  */
 #ifndef _FRAME_SPI_H_
@@ -14,6 +14,11 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+extern void Drv_SpiInit(void);
+extern void Drv_SpiTransData(uint8_t *pTxData, uint16_t size);
+extern void Drv_SpiRecvData(uint8_t *pRxData, uint16_t size);
+extern void Drv_SpiSpeedChanged(uint8_t speed);
 
 #ifdef __cplusplus
 }
--- SPI/spi_msp430.c
+++ SPI/spi_msp430.c
@@ -2,7 +2,167 @@
  * @Description: msp430 spi
  * @Author: land sea
  * @Date: 2019-11-18 20:43:34
- * @LastEditTime: 2019-11-18 20:47:52
+ * @LastEditTime: 2019-11-18 20:58:50
  * @LastEditors: Please set LastEditors
  */
+
 #include "spi.h"
+#include "driverlib.h"      //MSP430底层包
+
+/**
+ * @description: SPI初始化
+ * @param {type} 
+ * @return: 
+ */
+void Drv_SpiInit(void)
+{
+    GPIO_setAsPeripheralModuleFunctionInputPin(GPIO_PORT_P1, GPIO_PIN7, GPIO_SECONDARY_MODULE_FUNCTION);
+    GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_P1, GPIO_PIN6, GPIO_SECONDARY_MODULE_FUNCTION);
+    GPIO_setAsPeripheralModuleFunctionOutputPin(GPIO_PORT_P2, GPIO_PIN2, GPIO_SECONDARY_MODULE_FUNCTION);
+
+    EUSCI_B_SPI_initMasterParam param = {0};
+    param.selectClockSource = EUSCI_B_SPI_CLOCKSOURCE_SMCLK;
+    param.clockSourceFrequency = CS_getSMCLK();
+
+    param.desiredSpiClock = 1000000;
+    param.msbFirst = EUSCI_B_SPI_MSB_FIRST;
+    
+    param.clockPhase = EUSCI_B_SPI_PHASE_DATA_CAPTURED_ONFIRST_CHANGED_ON_NEXT;
+    param.clockPolarity = EUSCI_B_SPI_CLOCKPOLARITY_INACTIVITY_LOW;
+
+    param.spiMode = EUSCI_B_SPI_3PIN;
+    EUSCI_B_SPI_initMaster(EUSCI_B0_BASE, &param);
+
+    //Enable SPI module
+    EUSCI_B_SPI_enable(EUSCI_B0_BASE);
+}
+
+/**
+ * @description: SPI主设备发出函数
+ * @param {type} 
+ * @return: 
+ */
+void Drv_SpiTransData(UINT8 *pTxData, UINT16 size)
+{
+    if((pTxData == NULL)||(size == 0))
+    {
+        return;
+    }
+
+    UINT16 wsize = size;
+    UINT8 *bydata = pTxData;
+
+    while (wsize)
+    {
+        if (!(UCBUSY & UCB0STATW))
+        {
+            UCB0TXBUF = (*bydata++);
+            wsize--;
+        }
+    }
+}
+/**
+ * @description: SPI主双设备接收函数
+ * @param {type} 
+ * @return: 
+ */
+void Drv_SpiRecvData(UINT8 *pRxData, UINT16 size)
+{
+    if((pRxData == NULL)||(size <= 0))
+    {
+        return;
+    }
+
+    UINT16 wsize = size;
+    UINT8 *bydata = pRxData, byTxFlag = TRUE;
+
+    while (wsize)
+    {
+        if((byTxFlag)&&(!(UCBUSY & UCB0STATW)))
+        {
+            UCB0TXBUF = 0x00;
+            byTxFlag = FALSE;
+        }
+        if (!(UCBUSY & UCB0STATW))
+        {
+            *bydata++ = UCB0RXBUF;
+            if(--wsize)
+            {
+                byTxFlag = TRUE;
+            }
+        }
+    }
+}
+
+/**
+ * @description: SPI主设备接收和发送函数一体，但仅能接收或仅能发送
+ * @param {type} 
+ * @return: 
+ */
+void Drv_SpiSendAndRecv(UINT8 *pTxData,UINT8 *pRxData, UINT16 size)
+{
+    if(((pTxData == NULL)&&(pRxData == NULL))||(0 == size)||((pRxData != NULL)&&(pTxData != NULL)))
+    {
+        return;         //如果发送和接收缓冲区均为空，则返回
+    }
+
+    UINT16 TempSize = size;
+    UINT8 *ptempdata, byTxFlag = FALSE, byRxFlag = FALSE, byallowTx = FALSE;
+    if (pTxData != NULL)
+    {
+        byTxFlag = TRUE;
+        ptempdata = pTxData;
+    }
+    else
+    {
+        byallowTx = TRUE;
+        byRxFlag = TRUE;
+        ptempdata = pRxData;
+    }
+
+    while(TempSize)
+    {
+        if (((byallowTx)||(byTxFlag))&&(!(UCBUSY & UCB0STATW)))
+        {
+            if(byTxFlag)
+            {
+                UCB0TXBUF = (*ptempdata++);
+                TempSize--;
+            }
+            else
+            {
+                UCB0TXBUF = 0x00;
+                byallowTx = FALSE;
+            }
+        }
+
+        if ((byRxFlag)&&(!(UCBUSY & UCB0STATW)))
+        {
+            *ptempdata++ = UCB0RXBUF;
+            if(--TempSize)
+            {
+                byallowTx = TRUE;
+            }
+        }
+    }
+}
+
+/**
+ * @description: SPI速率调整
+ * @param {type} speed  SPI速率   单位为HZ
+ * @return: 
+ */
+void Drv_SpiSpeedChanged(UINT8 speed)
+{
+    EUSCI_B_SPI_changeMasterClockParam param = {0};
+    param.clockSourceFrequency = CS_getSMCLK();
+    param.desiredSpiClock = speed;
+
+    EUSCI_B_SPI_changeMasterClock(EUSCI_B0_BASE, &param);
+
+    // EUSCI_B_SPI_initMasterParam param = {0};
+    // param.desiredSpiClock = speed;
+    // EUSCI_B_SPI_initMaster(EUSCI_B0_BASE, &param);
+    // //Enable SPI module
+    // EUSCI_B_SPI_enable(EUSCI_B0_BASE);
+}
